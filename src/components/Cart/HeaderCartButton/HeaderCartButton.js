c8326a65ce9c5b13398d6619f1261718ad738da7
theflyingmachine101 <shikhasinghs216@gmail.com>
2021-08-04 22:04:30 +0530

    Bump feature added

--- src/components/Cart/HeaderCartButton/HeaderCartButton.js
+++ src/components/Cart/HeaderCartButton/HeaderCartButton.js
@@ -1,11 +1,27 @@
 import CartIcon from './CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../../store/cart-context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
 const HeaderCartButton = (props) => {
   const totalItems=useContext(CartContext).totalItems;
+  const [showBumps,setShowBumps]=useState(false);
+  const btnClasses = `${classes.button} ${ showBumps?classes.bump:""}`;
+
+  useEffect(()=>{
+    setShowBumps(true);
+
+    const timer = setTimeout(() => {
+      setShowBumps(false);
+    }, 300);
+
+    return  () => {
+      clearTimeout(timer);
+    };
+
+  },[totalItems]);
   return (
-    <button className={classes.button} onClick={props.CartModalHandler} >
+    <button className={btnClasses} onClick={props.CartModalHandler} >
       <span className={classes.icon}>
         <CartIcon />
       </span>
